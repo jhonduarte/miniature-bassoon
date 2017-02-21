df6b0c22b98cc8200b248c1a143f4befd5a625a5
jhonduarte <jduarte890@unab.edu.co>
2017-02-21 06:43:08 -0500

    Create productos.java

---
+++ productos.java
@@ -0,0 +1,20 @@
+package precios_Productos;
+import java.util.Scanner;
+
+public class productos {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int carne_res, carne_pollo, carne_pescado; //inicializa los precios en razón acomulado
+		Scanner lector = new Scanner(System.in);
+		System.out.println("Digite el valor de la carne de res");
+		carne_res = lector.nextInt();
+		System.out.println("Digite el valor de la carne de pollo");
+		carne_pollo = lector.nextInt();
+		System.out.println("Digite el valor de la carne de pescado");
+		carne_pescado = lector.nextInt();
+		
+	}
+
+}
