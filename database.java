1fce9318409a70f9e56f189b716f9940deede543
jhonduarte <jduarte890@unab.edu.co>
2017-03-21 10:13:05 -0500

    Update database.java

--- database.java
+++ database.java
@@ -1,1 +1,58 @@
-private static Connection Conexion;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class database {
+  private static Connection Conexion;
+  public void MySQLConnection(String user, String pass, String db_name) throws Exception {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + db_name, user, pass);
+            JOptionPane.showMessageDialog(null, "Se ha iniciado la conexión con el servidor de forma exitosa");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+  
+  //Metodo para crear la Base de datos
+   public void createDB(String name) {
+        try {
+            String Query = "CREAR BASE DE DATOS" + name;
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+            closeConnection();
+            MySQLConnection("admin", "", name);
+JOptionPane.showMessageDialog(null, "Se ha creado la base de datos " + name + " de forma exitosa");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+  
+  //
+   public void crearTabla(String name) {
+        try {
+            String Query = "Crear Tabla" + name + ""
+                    + " (ID VARCHAR(25),Producto VARCHAR(50),Cantidad VARCHAR(50),"
+                    + " PrecioUnitario VARCHAR(50), Precio Total VARCHAR(50))";
+
+            Statement st = Conexion.createStatement();
+            st.executeUpdate(Query);
+JOptionPane.showMessageDialog(null, "Se ha creado la tabla " + name + " de forma exitosa");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+  
+  /*//Metodo que finaliza la conexión con el servidor
+   public static void main(String[] args) {
+        MySQL db = new MySQL();
+        db.MySQLConnection("admin", "", "");
+    }
+  */
+}
